d81227410e3a5fb2290347fe33da713df520547d
Pit <drpitlazarus@gmail.com>
2025-01-21 22:21:44 -0500

    [app] Add error page, mainly to set the title

---
+++ src/routes/+error.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import type { PageData } from './$types';
+	import { page } from '$app/stores';
+
+	export let data: PageData;
+	let title = data.title;
+	$title = $page.status.toString();
+</script>
+
+<h1>{$page.status}</h1>
+<p>{$page.error?.message}</p>
